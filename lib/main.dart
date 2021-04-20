11ddf428683ac73b234191a32324f99e6ddd2231
vorishirne <vorishirne@gmail.com>
2021-04-20 09:46:20 +0530

    Reader's edits

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
       ),
       home: SafeArea(child: Text("okay")),
     );
